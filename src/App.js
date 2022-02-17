b3f7bbc6850505c6692f016df79e6d461e1cb0cf
Linlin Li <linlin.e.li@infinityworks.com>
2022-02-17 15:03:21 +0000

    style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import { fetchCountriesImplicitPromise } from "../api/functions";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  useParams,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+
+const Post = () => {
+  const { id } = useParams();
+  return <h2>This is Country Name: {id} </h2>;
+};
+const HomePage=()=><h1>I am at Home Page</h1>
+
+const Button = ({currentCountry,country,setCountry })=>{
+
+const navigate = useNavigate();
+
+
+  return  <button
+                className={`${
+                  currentCountry === country.Country ? "selected" : null
+                }`}
+                onClick={() => {
+                  setCountry(country.Country);
+                  navigate(`/country/${country.Country}`);
+                }}
+              >
+                {country.Country}
+              </button>
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [currentCountry, setCountry] = useState("");
+  
+  useEffect(() => {
+    fetchCountriesImplicitPromise().then(setCountries);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <section>
+          {countries.length &&
+            countries.map((country) => <Button currentCountry={currentCountry}  country={country} setCountry={setCountry}/>)}
+        </section>
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+ 
+          <Route path="/country/:id" element={<Post />}  exact/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
+
 
 export default App;
